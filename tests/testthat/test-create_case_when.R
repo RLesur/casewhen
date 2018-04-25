76bfdfab1b1033edf908053a55072af1f4ccb1eb
Romain Lesur <romain.lesur@gmail.com>
2018-04-25 20:52:13 +0200

    add closure test

--- tests/testthat/test-create_case_when.R
+++ tests/testthat/test-create_case_when.R
@@ -1,24 +1,17 @@
 context("test-create_case_when.R")
 
-vars <- c("x", "y", "z")
-
 test_that("x argument is a character vector", {
   expect_error(create_case_when(TRUE ~ as.character(x), vars = 1))
 })
 
-test_that("returned function has correct formals", {
-  expect_equal(
-    rlang::fn_fmls_names(
-      create_case_when(TRUE ~ as.character(x), vars = vars)
-    ),
-    vars
-  )
-  expect_length(
-    rlang::fn_fmls(create_case_when(TRUE ~ as.character(x), vars = vars)),
-    length(vars)
-  )
-  lapply(
-    rlang::fn_fmls(create_case_when(TRUE ~ as.character(x), vars = vars)),
-    function(x) expect_equal(x, rlang::missing_arg())
-  )
+vars <- c("x", "y", "z")
+cw <- create_case_when(TRUE ~ as.character(x), vars = vars)
+
+test_that("returned value is a closure", {
+  expect_type(cw, "closure")
+})
+
+test_that("returned function has correct formals with missing args", {
+  expect_equal(rlang::fn_fmls_names(cw), vars)
+  lapply(rlang::fn_fmls(cw), function(x) expect_equal(x, rlang::missing_arg()))
 })
