55621693a78607b3acf541c1a914cf9cd192ac7e
Romain Lesur <romain.lesur@gmail.com>
2018-04-25 03:06:41 +0200

    test formals

--- tests/testthat/test-create_case_when.R
+++ tests/testthat/test-create_case_when.R
@@ -3,3 +3,14 @@ context("test-create_case_when.R")
 test_that("x argument is a character vector", {
   expect_error(create_case_when(TRUE ~ as.character(x), vars = 1))
 })
+
+test_that("returned function has correct formals", {
+  expect_equal(
+    rlang::fn_fmls_names(
+      create_case_when(TRUE ~ as.character(x),
+                       vars = c("x", "y", "z")
+      )
+    ),
+    c("x", "y", "z")
+  )
+})
