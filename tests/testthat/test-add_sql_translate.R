a6c6d6b7ac36cd7b5a687b833786d8737a6080dd
Romain Lesur <romain.lesur@gmail.com>
2018-04-29 22:53:47 +0200

    add test for add_translate_sql

--- tests/testthat/test-add_sql_translate.R
+++ tests/testthat/test-add_sql_translate.R
@@ -1,5 +1,10 @@
 context("test-add_sql_translate.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(dplyr)
+library(dbplyr)
+
+test_that("translate sql works", {
+  cw <- create_case_when(x == 1 ~ TRUE)
+  add_sql_translate(cw)
+  expect_equal(translate_sql(case_when(y == 1 ~ TRUE)), translate_sql(cw(y)))
 })
