481feca320069b2907765e5c52859ad5410f46db
Romain Lesur <romain.lesur@gmail.com>
2018-04-30 02:10:42 +0200

    add one more test with SQLite

--- tests/testthat/test-add_sql_translate.R
+++ tests/testthat/test-add_sql_translate.R
@@ -11,11 +11,16 @@ test_that("translate sql works with con NULL", {
 
 test_that("translate sql works with SQLite", {
   cw2 <- create_case_when(y == 2 ~ TRUE, vars = "y")
-  con <- structure(
-    list(),
-    class = c("SQLiteConnection", "DBIConnection")
-  )
+  con <- simulate_sqlite()
   add_sql_translate(cw2, con = con)
   expect_equal(translate_sql(case_when(z == 2 ~ TRUE), con = con),
                translate_sql(cw2(z), con = con))
+
+  df <- memdb_frame(x = c(1, 2), y = c(3, 4))
+  cw3 <- create_case_when(y == 1 ~ TRUE, TRUE ~ FALSE, vars = "y")
+  add_sql_translate(cw3, con = con)
+  expect_equal(
+    capture.output(df %>% mutate(z = case_when(x == 1 ~ TRUE, TRUE ~ FALSE)) %>% show_query()),
+    capture.output(df %>% mutate(z = cw3(x)) %>% show_query())
+  )
 })
