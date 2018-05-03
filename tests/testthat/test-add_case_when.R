3bb9c4c9d9ae4bf2ed0987c5f990e48e7d7970f2
Romain Lesur <romain.lesur@gmail.com>
2018-05-04 01:56:45 +0200

    add more tests

--- tests/testthat/test-add_case_when.R
+++ tests/testthat/test-add_case_when.R
@@ -21,9 +21,37 @@ test_that("add_case_when works without dots argument", {
   expect_equal(con, con2)
 })
 
+test_that("Non case_when arguments are thrown by add_case_when", {
+  expect_warning(con2 <- add_case_when(con, sum, pi), regexp = "case_when")
+  expect_equal(con, con2)
+
+  expect_warning(con2 <- add_case_when(con, sum, cw_sex), regexp = "case_when")
+  expect_equal(length(get_case_when_funs(con2)), 1)
+})
+
+test_that("SQL reserved functions are thrown by add_case_when", {
+  expect_warning(con2 <- add_case_when(con, mean = cw_sex, mean = cw_sex), regexp = "reserved")
+  expect_equal(con, con2)
+
+  expect_warning(con2 <- add_case_when(con, cw_sex, mean = cw_sex), regexp = "reserved")
+  expect_equal(length(get_case_when_funs(con2)), 1)
+})
+
 test_that("new connection object class", {
   expect_s4_class(add_case_when(con, cw_sex), class(con))
   expect_s4_class(add_case_when(con, cw_sex), "CustomTranslation")
+})
+
+test_that("A previously sealed formal class does not lead to error", {
+  expect_silent(con2 <- add_case_when(con, cw_sex))
+  expect_silent(con3 <- add_case_when(con, cw_sex))
+  expect_equivalent(con2, con3)
+})
+
+test_that("One can reuse the new connection object", {
+  expect_silent(con2 <- add_case_when(con, cw_sex))
+  expect_silent(con3 <- add_case_when(con2, cw_sex2 = cw_sex))
+  expect_equal(length(get_case_when_funs(con3)), 2)
 })
 
 test_that("request with new conn leads to the same result than local computation", {
@@ -42,4 +70,9 @@ test_that("request with new conn leads to the same result than local computation
     collect()
 
   expect_equal(local_result, remote_result)
+})
+
+test_that("show method throws output", {
+  expect_silent(con2 <- add_case_when(con, cw_sex))
+  expect_output(show(con2), regexp = "<SQLiteConnection>")
 })
