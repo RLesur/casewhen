ac15478f94f0ba14e0c6316ba6a55b360527801c
Romain Lesur <romain.lesur@gmail.com>
2018-05-04 01:09:05 +0200

    implement a safely add_case_when

--- R/add_case_when.R
+++ R/add_case_when.R
@@ -3,17 +3,32 @@
 #' @importFrom dbplyr sql_variant sql_translator
 NULL
 
-setOldClass("sql_variant")
-
 cacheClasses <- new.env(parent = emptyenv())
 
-reserved_names <- names(dplyr::sql_translate_env(NULL))
-
+setOldClass("sql_variant")
 setClass("CustomTranslation", slots = list(sql_translate_env = "sql_variant"))
 
 #' @export
 sql_translate_env.CustomTranslation <- function(con) {
   con@sql_translate_env
+}
+
+original_sql_functions <- function(con) {
+  translate_env <- dplyr::sql_translate_env(con)
+  fn_list <- c(as.list(translate_env$scalar),
+               as.list(translate_env$aggregate),
+               as.list(translate_env$window)
+               )
+  names(fn_list[!is_case_when(fn_list)])
+}
+
+get_case_when_funs <- function(con) {
+  translate_env <- dplyr::sql_translate_env(con)
+  fn_list <- c(as.list(translate_env$scalar),
+               as.list(translate_env$aggregate),
+               as.list(translate_env$window)
+  )
+  fn_list[is_case_when(fn_list)]
 }
 
 #' Add a customised translation to a connection
@@ -31,13 +46,9 @@ add_case_when <-
 #' @export
 add_case_when.DBIConnection <- function(con, ...) {
   fn_list <- list(...)
-  if (length(fn_list) == 0) {
-    warning("No function to add. Returning original connection object...")
-    return(con)
-  }
-
   fn_names <- names(fn_list)
-  # Ensure that each element of fn_names has a name
+
+  # Ensure that each element of fn_names is named
   dots_args <- as.list(match.call()[-(1:2)])
   if (is.null(fn_names)) {
     names(fn_list) <- as.character(dots_args)
@@ -46,40 +57,78 @@ add_case_when.DBIConnection <- function(con, ...) {
     names(fn_list) <- ifelse(nzchar(fn_names), fn_names, as.character(dots_args))
   }
 
-  # TODO: don't throw an error
-  lapply(fn_list, function(x) assertthat::assert_that(inherits(x, "case_when")))
-
-  # TODO: don't throw an error
-  # Throw an error if a function's name is reserved
-  if (any(forbidden <- names(fn_list) %in% reserved_names))
-    stop(paste(names(fn_list)[forbidden], collapse = ", "), ": reserved word(s).")
-
-  if (!inherits(con, "CustomTranslation")) {
-    # Here, con is a pure DBIConnection
-    connection_class <- class(con)
-    new_class <- paste0("CustomisedTranslation", connection_class)
-    if (!isClass(new_class))
-      setClass(new_class,
-               contains = c("CustomTranslation", connection_class),
-               where = cacheClasses
-      )
-  } else {
-    new_class <- class(con)
+  # Drop non case_when functions with warning
+  is_cw <- is_case_when(fn_list)
+  if (any(!is_cw)) {
+    warning(paste(names(fn_list[!is_cw]), collapse = ", "),
+            " dropped: not case_when function(s).\n"
+            )
+    fn_list <- fn_list[is_cw]
   }
 
-  translate_env <- dplyr::sql_translate_env(con)
+  # Drop functions corresponding to a SQL name with warning
+  reserved_names <- original_sql_functions(con)
+  forbidden <- names(fn_list) %in% reserved_names
+  if (any(forbidden)) {
+    warning(paste(names(fn_list)[forbidden], collapse = ", "),
+            " dropped: reserved SQL function(s).\n")
+    fn_list <- fn_list[!forbidden]
+  }
 
-  variant_translate_env <- dbplyr::sql_variant(
-    scalar = dbplyr::sql_translator(
-      .parent = translate_env$scalar,
-      .funs = .translate_to_sql(fn_list, con = con)
-    ),
-    aggregate = translate_env$aggregate,
-    window = translate_env$window
-  )
+  if (length(fn_list) == 0) {
+    warning("No function to add. Returning original connection object...\n")
+    return(con)
+  }
 
-  custom_translation <- new("CustomTranslation",
-                            sql_translate_env = variant_translate_env
-                            )
-  new(new_class, custom_translation, con)
+  # Create a new class and a new connection object with custom translation
+  # These operations are embedded in a tryCatch():
+  #   if an error occurs, the original con object is returned
+  # This tryCatch() ensures a safe use of add_case_when with a pipe
+  tryCatch(
+    {
+      # Test if con is an original connection
+      if (!inherits(con, "CustomTranslation")) {
+        connection_class <- class(con)
+        new_class <- paste0("CustomisedTranslation", connection_class)
+        # Create a new formal class
+        if (!isClass(new_class, where = cacheClasses))
+          setClass(new_class,
+                   contains = c("CustomTranslation", connection_class),
+                   where = cacheClasses
+          )
+      } else {
+        new_class <- class(con)
+      }
+
+      translate_env <- dplyr::sql_translate_env(con)
+
+      variant_translate_env <- dbplyr::sql_variant(
+        scalar = dbplyr::sql_translator(
+          .parent = translate_env$scalar,
+          .funs = .translate_to_sql(fn_list, con = con)
+        ),
+        aggregate = translate_env$aggregate,
+        window = translate_env$window
+      )
+
+      custom_translation <- new("CustomTranslation",
+                                sql_translate_env = variant_translate_env
+      )
+      new(new_class, custom_translation, con)
+    },
+    error = function(e) {
+      warning("An error has occurred.\nError message:\n", e$message,
+              "\nReturning original connection object...\n"
+             )
+      con
+  })
 }
+
+#' @param object A customised connection.
+#' @rdname add_case_when.DBIConnection
+#' @export
+setMethod("show", "CustomTranslation", function(object) {
+  con_class <- sub("CustomisedTranslation", "", class(object))
+  getMethod("show", con_class)(object)
+  print(get_case_when_funs(object))
+})
